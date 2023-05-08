6123d9bf0173cda7e7501bc39dd287d849d64cbc
Tom <petridistom@gmail.com>
2023-05-08 17:27:32 +1000

    fnew local address

--- main.ts
+++ main.ts
@@ -6,4 +6,4 @@ const serveFiles = (req: Request) => staticFiles('public')({
     respondWith: (r: Response) => r 
 })
 
-serve((req) => serveFiles(req), { addr: ':8000' });
+serve((req) => serveFiles(req), { addr: ':8800' });
