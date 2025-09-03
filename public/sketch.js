f523c942b2e9b95cb89418941a3ae1ba5d537abf
tom petridis <110432739+Petridistom@users.noreply.github.com>
2025-09-03 11:36:53 +1000

    modernized the code with chatgpt

--- public/sketch.js
+++ public/sketch.js
@@ -1,139 +1,113 @@
-let sprites; // initiate variable sprite
-let amount_sprites = 110 // determines amount of sprites
+let sprites; 
+let amount_sprites = 110;
 
-function setup() {
-  ghost = loadImage('images/ghost.png')
-  ghost_flipped = loadImage('images/ghostFlipped.png')
-  ghostSad = loadImage('images/ghostSad.png')
-  open = loadImage('images/open.png')
-  shut = loadImage('images/shut.png')
+// ghost images
+let ghost, ghost_flipped, ghostSad, openEye, shutEye;
 
-  eye = open
-  
-  createCanvas(windowWidth / 1.2, windowHeight / 1.2);
-  noStroke()
-  noCursor()
-  
-  let posx // initiate coordinates x, y
-  let posy
-  
-  sprites = new Group() // create a sprite group and 
-                        // define sprite properties
-  while (sprites.length <= amount_sprites) {
-    let sprite = new sprites.Sprite(random(width), random(height), 40, 40)
-    sprite.img = eye;
-    sprite.rotationLock = true;
-    sprite.acceleration = createVector()
-    sprite.velocity = createVector()
-    sprite.postition = createVector (posx, posy)
-    sprite.maxDist = 100
-    sprite.maxForce = 5
-    sprite.dampening = 0.1
-    sprite.debug = mouse.pressing()
-
-
-    sprite.flipEye = e => {
-
-      if (mouseX > sprite.position.x - 150 && mouseX < sprite.position.x + 150 && mouseY > sprite.position.y - 150 && mouseY < sprite.position.y + 150) {
-        eye = shut
-        sprite.img = eye
-      } else {
-        eye = open
-        sprite.img = eye
-      }
-      }
-    }
+function preload() {
+  ghost = loadImage('images/ghost.png');
+  ghost_flipped = loadImage('images/ghostFlipped.png');
+  ghostSad = loadImage('images/ghostSad.png');
+  openEye = loadImage('images/open.png');
+  shutEye = loadImage('images/shut.png');
 }
 
+function setup() {
+  createCanvas(windowWidth / 1.2, windowHeight / 1.2);
+  noStroke();
+  noCursor();
 
-function draw() { // calls each function
-                  // once per frame
-  // functions are 
-  // defined below
-  changeEye();  
-  admin();
+  sprites = new Group();
+
+  for (let i = 0; i < amount_sprites; i++) {
+    let s = new Sprite(random(width), random(height), 40, 40);
+    s.img = openEye;
+    s.rotationLock = true;
+
+    // custom properties
+    s.velocity = createVector();
+    s.acceleration = createVector();
+    s.maxDist = 100;
+    s.maxForce = 5;
+    s.dampening = 0.1;
+
+    // eye-flip behavior
+    s.flipEye = () => {
+      if (
+        abs(mouseX - s.position.x) < 150 &&
+        abs(mouseY - s.position.y) < 150
+      ) {
+        s.img = shutEye;
+      } else {
+        s.img = openEye;
+      }
+    };
+
+    sprites.add(s);
+  }
+}
+
+function draw() {
+  background(0);
+  drawSprites();   // p5.play renderer
+
+  changeEye();
   repel();
   bounce();
   flipCursor();
-
-  
 }
 
-function admin() { // draws the background
-                   // and the sprites
-  background(0);
-  drawSprites();
-}
-
+// 👻 ghost cursor
 function flipCursor() {
-  //check which way the mouse is moving
-  if (mouseX - pmouseX < 0) {
-    image(ghost, mouseX, mouseY, 50, 50)
-    setTimeout(1000)
-  }
-  else if (mouseX - pmouseX > 0) {
-    image(ghost_flipped, mouseX, mouseY, 50, 50)
-    setTimeout(1000)
-  } 
-  else {
-    image(ghostSad, mouseX, mouseY, 50, 50)  
-    setTimeout(1000)
+  let dx = mouseX - pmouseX;
+
+  if (dx < 0) {
+    image(ghost, mouseX, mouseY, 50, 50);
+  } else if (dx > 0) {
+    image(ghost_flipped, mouseX, mouseY, 50, 50);
+  } else {
+    image(ghostSad, mouseX, mouseY, 50, 50);
   }
 }
 
-function repel() { // updates velocity values
-                  // for sprites near the mouse
-    for(let i = 0; i < sprites.length; i++) {
-    
-    sprites[i].mousePosition = createVector(mouseX, mouseY)
-    
-    sprites[i].acceleration = p5.Vector.sub(sprites[i].position, sprites[i].mousePosition).setMag(
-    map(sprites[i].position.dist(sprites[i].mousePosition), 0, sprites[i].maxDist, sprites[i].maxForce, 0, true)
-    );
-    
-    sprites[i].velocity.add(sprites[i].acceleration);
-    
-    sprites[i].position.add(sprites[i].velocity.mult(1-sprites[i].dampening));
-  }
-}
-
-function changeEye () {
+// 👀 eye behavior
+function changeEye() {
   sprites.forEach((s) => {
-    s.flipEye()
-  })
+    s.flipEye();
+  });
 }
 
-function bounce() { // keeps sprites bounded
-                    // inside the page
-  
-  for (let i = 0; i < sprites.length; i++) {
-   if (sprites[i].position.x - 40 / 2 < 1) {
-   //keep the circle on the page
-   //change its direction
-    sprites[i].position.x = 40 - sprites[i].position.x; 
-    sprites[i].velocity.x *= -1;
-  } 
-    
-    else if (sprites[i].position.x + 40 / 2 > width -1) {
-      //keep the circle on the page
-      //change its direction
-    sprites[i].position.x = 2 * (width - 1) - 40 - sprites[i].position.x;
-    sprites[i].velocity.x *= -1;
-  }
-    
-    
-  if (sprites[i].position.y - 40 / 2 < 1) {
-      //keep the circle on the page
-      //change its direction
-    sprites[i].position.y = 40 - sprites[i].position.y;
-    sprites[i].velocity.y *= -1;
-  } 
-    
-    else if (sprites[i].position.y + 40 / 2 > height - 1) {
-      //keep the circle on the page
-      //change its direction
-    sprites[i].position.y = 2 * (height - 1) - 40 - sprites[i].position.y;
-    sprites[i].velocity.y *= -1;
-  }
-}    
+// 🌀 repel sprites from mouse
+function repel() {
+  sprites.forEach((s) => {
+    let mousePos = createVector(mouseX, mouseY);
+    s.acceleration = p5.Vector.sub(s.position, mousePos).setMag(
+      map(s.position.dist(mousePos), 0, s.maxDist, s.maxForce, 0, true)
+    );
+
+    s.velocity.add(s.acceleration);
+    s.position.add(s.velocity.mult(1 - s.dampening));
+  });
+}
+
+// 🔄 keep sprites inside canvas
+function bounce() {
+  sprites.forEach((s) => {
+    if (s.position.x - s.w / 2 < 0) {
+      s.position.x = s.w / 2;
+      s.velocity.x *= -1;
+    }
+    if (s.position.x + s.w / 2 > width) {
+      s.position.x = width - s.w / 2;
+      s.velocity.x *= -1;
+    }
+    if (s.position.y - s.h / 2 < 0) {
+      s.position.y = s.h / 2;
+      s.velocity.y *= -1;
+    }
+    if (s.position.y + s.h / 2 > height) {
+      s.position.y = height - s.h / 2;
+      s.velocity.y *= -1;
+    }
+  });
 }
